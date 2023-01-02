0d8428c8dd3ac2b606360ffd7ff9e6b0193f95cb
Recep Tiryaki <recep.tiryaki@gmail.com>
2023-01-02 13:24:38 +0300

    v

---
+++ packages/sub/dist/index.js
@@ -0,0 +1,21 @@
+function $parcel$defineInteropFlag(a) {
+  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
+}
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$defineInteropFlag(module.exports);
+
+$parcel$export(module.exports, "default", () => $7288f5d779de7a24$export$2e2bcd8739ae039);
+/**
+ * Subtracts b from a.
+ * @constructor
+ * @param {number} a - Your number.
+ * @param {number} b - To subtract.
+ */ function $7288f5d779de7a24$export$2e2bcd8739ae039(a, b) {
+    return a - b;
+}
+
+
+//# sourceMappingURL=index.js.map
